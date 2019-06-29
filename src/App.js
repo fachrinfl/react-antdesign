b421ae888134dae100537d86cf4235818c1d38eb
Fachri Naufal <fachrinfl@gmail.com>
2019-06-29 22:56:43 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Button, Table, Divider, Tag} from 'antd';
+import 'antd/dist/antd.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  render() {
+    const {Content} = Layout;
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'Hobby',
+        dataIndex: 'hobby',
+        key: 'hobby',
+      },
+      {
+        title: 'Category',
+        dataIndex: 'category',
+        key: 'category',
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+            <span>
+        <a href="javascript:;">Edit</a>
+        <Divider type="vertical" />
+        <a href="javascript:;">Delete</a>
+      </span>
+        ),
+      }
+    ];
+
+    const data = [
+      {
+        key: '1',
+        name: 'Sonie',
+        hobby: 'Mobile Legend',
+        category: 'Game',
+      },
+      {
+        key: '2',
+        name: 'Isgie',
+        hobby: 'Futsal',
+        category: 'Olahraga',
+      }
+    ];
+    return (
+        <Layout className="layout">
+          <Content style={{ padding: '50px' }}>
+            <div style={{marginBottom: '20px'}}>
+              <Button type="primary">Add</Button>
+            </div>
+            <div>
+              <Table columns={columns} dataSource={data} pagination={false} style={{background: '#ffffff'}}/>
+            </div>
+          </Content>
+        </Layout>
+    );
+  }
 }
 
 export default App;
